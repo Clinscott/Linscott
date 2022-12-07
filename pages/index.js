8d1bc661f4a1d8b70598b0c2b9c5fb56936620ff
Clinscott <clinscott@gmail.com>
2022-12-07 12:59:50 -0500

    working it

--- pages/index.js
+++ pages/index.js
@@ -31,24 +31,52 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <div className={utilStyles.rowC2}>
-        {/* <Image
-          priority
-          src="/images/NH-CraigFull.gif"
-          className={utilStyles.borderCircle}
-          height={founderImage.height}
-          width={founderImage.width}
-          alt="Craig Linscott"
-          styles={utilStyles.image}
-        /> */}
-        <section className={utilStyles.headingMd}>
-          <h4>Welcome, stay awhile and read.</h4>
-          <></>
-        </section>
-      </div>
+      {/* <div className={utilStyles.rowC2}>*/}
+      <section className={utilStyles.headingMd}>
+        <h4>White Hat. Developer. Writer.</h4>
+      </section>
+      <section
+        className={`${utilStyles.headingMd} ${utilStyles.floatContainer}`}
+      >
+        <div className={utilStyles.floatChild}>
+          <h6>Fraud Finder</h6>
+          <p>
+            Above are links to my major projects and my resume. Fraud Finder is
+            a website fraud finder in the vein of
+            <Link href={"https://github.com/elceef/dnstwist"}>
+              <a target="_blank"> DNS TWIST. </a>
+            </Link>
+            The code for Fraud Finder is entirely mine. Check it out!
+          </p>
+        </div>
+        <div className={utilStyles.floatChild}>
+          <h6>TriFighter</h6>
+          <p>
+            TriFighter is a fun little game I am developing in{" "}
+            <Link href={"https://phaser.io/"}>
+              <a target="_blank"> Phaser. </a>
+            </Link>
+            It is a continuous 2D base defence game where TriAngle runs around
+            punching evil Squares that are sent by the One of Infinite Angles,
+            The Circle. Eventually the game will apply upgrades and a saving
+            mechanism.
+          </p>
+        </div>
+        <div className={utilStyles.floatChild}>
+          <h6>Blog</h6>
+          <p>
+            Below are a series of blog posts that include thoughts on many
+            things that interest me. They range from learning to program,
+            warhammer, politics and writing. If this is of interest to you have
+            a look!
+          </p>
+        </div>
+      </section>
+      {/* </div> */}
       <section
         className={`${utilStyles.headingMd} ${utilStyles.padding1px} ${utilStyles.blogArticle}`}
       >
+        <h1>Rambles</h1>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title, author }) => (
             <li className={utilStyles.listItem} key={id}>
@@ -60,7 +88,7 @@ export default function Home({ allPostsData }) {
                         ? "/images/NH-Craig.gif"
                         : "/images/NH-Brad.gif"
                     }
-                    alt='Pixel Craig.'
+                    alt="Pixel Craig."
                     width={profilePic.width}
                     height={profilePic.height}
                   />
